7cd90ac836ee8b321f869d9d258c47344c5a8c75
JMarlow86 <98555094+JMarlow86@users.noreply.github.com>
2022-06-04 14:40:15 -0700

    organizing

--- orderform.js
+++ orderform.js
@@ -1,32 +1,26 @@
 
-
-
-
-   
-
-   
-   function play() {
-     var audio = document.getElementById("audio");
-     audio.play();
-   }
+function play() {
+  var audio = document.getElementById("audio");
+  audio.play();
+}
  
- 
+function myFunction() {
+  var x = document.createElement("INPUT");
+  x.setAttribute("type", "date");
+  x.setAttribute("value", "2014-02-09");
+  document.body.appendChild(x);
+}
 
-              function myFunction() {
-                var x = document.createElement("INPUT");
-                x.setAttribute("type", "date");
-                x.setAttribute("value", "2014-02-09");
-                document.body.appendChild(x);
-              }
-              function geekConfirm() {
-                var x;
-                if (confirm("Press a button!") == true) {
-                    x = "OK pressed!";
-                } else {
-                    x = "Cancel!";
-                }
-                document.getElementById("geek").innerHTML = x;
-            }
+function geekConfirm() {
+  var x;
 
-            var modal = document.getElementById("myModal");
+  if (confirm("Press a button!") == true) {
+      x = "OK pressed!";
+      } else {
+        x = "Cancel!";
+      }
+        document.getElementById("geek").innerHTML = x;
+      }
+      var modal = document.getElementById("myModal");
 
+    
