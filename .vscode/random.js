a4c68878297834a46af52b9dfe9d15d6421c6829
JMarlow86 <98555094+JMarlow86@users.noreply.github.com>
2022-06-05 08:25:03 -0700

    added random generator

--- .vscode/random.js
+++ .vscode/random.js
@@ -1,4 +1,4 @@
-// Get all the images
+
 image_array = [
     '1.jpg',
     '2.jpg',
@@ -11,13 +11,11 @@ image_array = [
   ]
   
   function get_random_image(){
-    // Get a random index
+    
     random_index = Math.floor(Math.random() * image_array.length);
-  
-    // Get an image at the random_index
+    
     selected_image = image_array[random_index]
   
-    // Display the image
     document.getElementById('image_shower').src = `./images/${selected_image}`
   }
 
--- orderform.js
+++ orderform.js
@@ -11,7 +11,18 @@ dateObjectValue.textContent = dateStringInput.valueAsDate.toString();
 
 dateObjectInput.addEventListener('input', () => {
   dateObjectValue.textContent = dateObjectInput.valueAsDate.toString();
+  
 });
 
+// function displayMessage() {
+//   var success = document.getElementById("btnSubmit");
 
-    
+  
+// }
+
+// Add a click event listener to the element.
+// Assign the event object to a variable in the function.
+// Set the event. target. style. color property to the specific text color.
+
+
+    document.getElementById(btnSubmit).addEventListener("click", function () {alert(success); });
